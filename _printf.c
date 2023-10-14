3dde53827ff110e298735f55f8d590ad9581d557
Youssef Hussein <49595721+yh919@users.noreply.github.com>
2023-10-15 01:43:10 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,37 +1,76 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * _printf - Custom printf function
+ * @format: The format string
+ * @...: Additional arguments based on the format
+ * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int printed_chars;
-	conver_t f_list[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{NULL, NULL}
-	};
-	va_list arg_list;
+    int count = 0;
+    va_list args;
+    int i = 0;
 
-	if (format == NULL)
-		return (-1);
+    va_start(args, format);
+    
+    while (format[i])
+    {
+        if (format[i] != '%')
+        {
+            count += _putchar(format[i]);
+        }
+        else
+        {
+            i++; // Move past '%'
+            if (format[i] == 'c')
+                count += print_char(va_arg(args, int));
+            else if (format[i] == 's')
+                count += print_str(va_arg(args, char *));
+            else if (format[i] == '%')
+                count += _putchar('%');
+            // Add more cases for other conversion specifiers here
+        }
+        i++; // Move to the next character in the format string
+    }
+    
+    va_end(args);
+    return count;
+}
 
-	va_start(arg_list, format);
-	/*Calling parser function*/
-	printed_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
+ * print_char - Print a character
+ */
+int print_char(char c)
+{
+    return _putchar(c);
+}
+
+/**
+ * print_str - Print a string
+ */
+int print_str(char *str)
+{
+    int count = 0;
+    if (str == NULL)
+        str = "(null)";
+    
+    while (str[count])
+    {
+        _putchar(str[count]);
+        count++;
+    }
+    return count;
 }
