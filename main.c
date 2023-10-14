58c40a2b2050bb3c159df5a193554bf1a953ce44
Youssef Hussein <youssefalcon30@gmail.com>
2023-10-15 01:49:43 +0300

    x2

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+
+    // Test other conversion specifiers and cases here...
+
+    return (0);
+}
