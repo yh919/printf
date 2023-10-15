f22d171318931b52857a245fa22a64fba0895769
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 15:42:56 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -12,57 +12,43 @@
 
 int _printf(const char *format, ...)
 {
-	/* Declare printf_37 */
 int printf_37(va_list val);
-
-/* Initialize the structure with an initializer list */
-
 convert_match m[] = {
-    {"%s", printf_string}, {"%c", printf_char},
-    {"%%", printf_37},
-    {"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-    {"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-    {"%o", printf_oct}, {"%x", printf_hex},{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+{"%s", printf_string}, {"%c", printf_char}, {"%%", printf_37},
+{"%i", printf_int}, {"%d", printf_dec},
+{"%r", printf_srev}, {"%R", printf_rot13},
+{"%b", printf_bin}, {"%u", printf_unsigned}, {"%o", printf_oct},
+{"%x", printf_hex}, {"%S", printf_exclusive_string}, {"%p", printf_pointer}
 };
-
-
 	va_list args;
-    int len = 0;
-    int i = 0;
-	int j = 0;
+	int i = 0, j = 0, len = 0;
 
 	va_start(args, format);
-
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
-	while (format[i] != '\0')
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			j = 0;
-			while (m[j].id != NULL)
+			for (j = 0; m[j].id != NULL; j++)
 			{
 				if (m[j].id[0] == format[i])
 				{
 					len += m[j].f(args);
 					break;
 				}
-				j++;
 			}
 			if (m[j].id == NULL)
 			{
 				_putchar('%');
 				len++;
 			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			len++;
-		}
-		i++;
+		} else
+			{
+				_putchar(format[i]);
+				len++;
+			}
 	}
 	va_end(args);
 	return (len);
--- printf_char.c
+++ printf_char.c
@@ -12,6 +12,7 @@
 int printf_char(va_list val)
 {
 	char c = va_arg(val, int);
+
 	_putchar(c);
 	return (1);
 }
