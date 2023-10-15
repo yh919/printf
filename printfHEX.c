33e9bdcc8d6ef6d46c03b7deb9b994fb3b9f8988
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 16:03:59 -0700

    printf_HEX

--- printfHEX.c
+++ printfHEX.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * printf_HEX - print hex
+ *
+ * @args: arguments
+ *
+ * Return: count
+ */
 
 int printf_HEX(va_list args)
 {
-    	char hexDigits[] = "0123456789abcdef";
+	char hexDigits[] = "0123456789abcdef";
 
 	unsigned int num = va_arg(args, unsigned int);
 	int count = 0;
@@ -13,16 +20,17 @@ int printf_HEX(va_list args)
 	if (num == 0)
 	{
 		_putchar('0');
-		return 1;
+		return (1);
 	}
 
 	while (num > 0)
 	{
 		int digit = num % 16;
+
 		_putchar(hexDigits[digit]);
 		num /= 16;
 		count++;
 	}
 
-	return count;
+	return (count);
 }
