8e803e79e597617e6ec4eee5ae01b9e44511edb5
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 16:24:29 -0700

    string

--- printf_string.c
+++ printf_string.c
@@ -1,29 +1,33 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
  * printf_string - Print a string.
+ *
  * @val: Argument.
+ *
  * Return: The length of the string.
  */
+
 int printf_string(va_list val)
 {
-    char *s;
-    int len;  /* Move 'len' outside the 'if' statement */
-    int i;
+	char *s;
+	int len;  /* Move 'len' outside the 'if' statement */
+	int i;
 
-    s = va_arg(val, char *);
-    len = _strlen(s);  /* Move 'len' outside the 'if' statement */
-    if (s == NULL)
-    {
-        s = "(null)";
-        for (i = 0; i < len; i++)
-            _putchar(s[i]);
-        return len;
-    }
-    else
-    {
-        for (i = 0; i < len; i++)
-            _putchar(s[i]);
-        return len;
-    }
+	s = va_arg(val, char *);
+	len = _strlen(s);  /* Move 'len' outside the 'if' statement */
+	if (s == NULL)
+	{
+		s = "(null)";
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		for (i = 0; i < len; i++)
+		_putchar(s[i]);
+		return (len);
+	}
 }
