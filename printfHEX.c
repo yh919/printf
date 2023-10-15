7dfd850b5725f221ff1314b5d98e304342bb00d8
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 14:59:22 -0700

    printfHEX

--- printfHEX.c
+++ printfHEX.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 #include <stddef.h>
+
 
 int printf_HEX(va_list args)
 {
-    char hexDigits[] = "0123456789abcdef";
+    	char hexDigits[] = "0123456789abcdef";
 
-unsigned int num = va_arg(args, unsigned int);
+	unsigned int num = va_arg(args, unsigned int);
 	int count = 0;
 
 	if (num == 0)
@@ -14,8 +15,6 @@ unsigned int num = va_arg(args, unsigned int);
 		_putchar('0');
 		return 1;
 	}
-
-
 
 	while (num > 0)
 	{
