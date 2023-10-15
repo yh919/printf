eeab93ab06f4517d525632596fb08cc7d5a8661a
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 15:54:23 -0700

    exclusive

--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -10,38 +10,40 @@
 
 int printf_exclusive_string(va_list val)
 {
-    char *s;
-    int i = 0;  /* Declare 'i' outside the 'for' loop */
-    int len = 0;
-    int cast;
+	char *s;
+	int i = 0;  /* Declare 'i' outside the 'for' loop */
+	int len = 0;
+	int cast;
 
-    s = va_arg(val, char *);
-    if (s == NULL)
-        s = "(null)";
-    for (; s[i] != '\0'; i++)
-    {
-        if (s[i] < 32 || s[i] >= 127)
-        {
-            _putchar('\\');
-            _putchar('x');
-            len = len + 2;
-            cast = s[i];
-            if (cast < 16)
-            {
-                _putchar('0');
-                len++;
-            }
-            len = len + _putchar_hex(cast);
-        }
-        else
-        {
-            _putchar(s[i]);
-            len++;
-        }
-    }
-    return len;
+	s = va_arg(val, char *);
+	if (s == NULL)
+	s = "(null)";
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+		_putchar('\\');
+		_putchar('x');
+		len = len + 2;
+		cast = s[i];
+
+		if (cast < 16)
+		{
+			_putchar('0');
+			len++;
+		}
+		len = len + _putchar_hex(cast);
+	}
+	else
+	{
+		{
+		_putchar(s[i]);
+		len++;
+		}
+	}
+	return (len);
+	}
 }
-
 
 /**
  * _putchar_hex - print a hexadecimal value for a character.
@@ -55,11 +57,11 @@ int _putchar_hex(char c)
 {
 	int len = 0;
 
-
 	char hexDigits[] = "0123456789ABCDEF";
+
 	_putchar(hexDigits[(c >> 4) & 0xF]);
 	_putchar(hexDigits[c & 0xF]);
 	len += 2;
 
-	return len;
+	return (len);
 }
