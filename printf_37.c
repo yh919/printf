eeb4c98a996c6deedf8541de04a2710de829a761
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 15:05:31 -0700

    37

--- printf_37.c
+++ printf_37.c
@@ -1,8 +1,12 @@
 #include "main.h"
 #include <stddef.h>
+/**
+ * printf_37 - print %
+ * Return: 1.
+ */
 
 int printf_37(void)
 {
-    _putchar('%');  
-    return (1);    
+	_putchar('%');
+	return (1);
 }
