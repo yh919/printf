2ec8195085d2dbee4ffc66b569f243a5d4a213ec
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 14:51:18 -0700

    char

--- printf_char.c
+++ printf_char.c
@@ -1,10 +1,14 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
  * printf_char - prints a character.
+ *
  * @val: arguments.
+ *
  * Return: Number of characters printed (always 1).
  */
+
 int printf_char(va_list val)
 {
 	char c = va_arg(val, int);
