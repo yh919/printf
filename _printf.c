17fdba14751491ff955e14d86812485a32184d34
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 14:49:03 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,14 +1,15 @@
-#include <stdarg.h>
-#include <stddef.h>
 #include "main.h"
-
-
+#include <stddef.h>
+#include <stdarg.h>
 
 /**
  * _printf - is a function that selects the correct function to print.
+ *
  * @format: identifier to look for.
+ *
  * Return: the length of the string.
  */
+
 int _printf(const char *format, ...)
 {
 	/* Declare printf_37 */
