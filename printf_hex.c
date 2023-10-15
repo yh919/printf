f8f40838cef78fac5f39b9d25811c32207ce7b6d
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 14:57:25 -0700

    hex

--- printf_hex.c
+++ printf_hex.c
@@ -1,13 +1,17 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
  * printf_hex - prints a hexadecimal number.
+ *
  * @val: arguments.
+ *
  * Return: Number of characters printed.
  */
+
 int printf_hex(va_list val)
 {
-		char hexDigits[] = "0123456789abcdef";
+	char hexDigits[] = "0123456789abcdef";
 
 	unsigned int num = va_arg(val, unsigned int);
 	int count = 0;
