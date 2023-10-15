4c5e5617896e53acb5ff8226569510d7dfa3dd35
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 16:12:25 -0700

    oct

--- printf_oct.c
+++ printf_oct.c
@@ -1,10 +1,14 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
  * printf_oct - prints an octal number.
+ *
  * @val: arguments.
+ *
  * Return: Number of characters printed.
  */
+
 int printf_oct(va_list val)
 {
 	unsigned int num = va_arg(val, unsigned int);
@@ -13,16 +17,17 @@ int printf_oct(va_list val)
 	if (num == 0)
 	{
 		_putchar('0');
-		return 1;
+		return (1);
 	}
 
 	while (num > 0)
 	{
 		int digit = num % 8;
+
 		_putchar(digit + '0');
 		num /= 8;
 		count++;
 	}
 
-	return count;
+	return (count);
 }
