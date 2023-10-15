b1c7cd1313d82b88ad5ef92d114f6e7fa2462ad4
Youssef Hussein <youssefalcon30@gmail.com>
2023-10-15 03:35:48 +0300

    zx

--- main.h
+++ main.h
@@ -29,7 +29,6 @@ int printf_unsigned(va_list val);
 int printf_bin(va_list val);
 int printf_oct(va_list val);
 int printf_hex(va_list val);
-int printf_hex_aux(unsigned long int num);
 int printf_exclusive_string(va_list val);
 int printf_pointer(va_list val);
 int printf_rot13(va_list val);
