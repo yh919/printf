ae12a75282bd90dfc84846d914680e8977f01d1f
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 14:55:30 -0700

    hex_aux

--- printf_hex_aux.c
+++ printf_hex_aux.c
@@ -1,14 +1,17 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
  * printf_hex_aux - prints a hexadecimal number.
+ *
  * @num: the number to print in hexadecimal.
+ *
  * Return: Number of characters printed.
  */
-int printf_hex_aux(unsigned long int num)
 
+int printf_hex_aux(unsigned long int num)
 {
-		char hexDigits[] = "0123456789abcdef";
+	char hexDigits[] = "0123456789abcdef";
 	int counter = 0;
 	unsigned long int temp = num;
 
