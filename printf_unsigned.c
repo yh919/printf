8d45673eb3141477249f234b6a5b107a8e31155a
YoussefAhmed52 <yousef3012001@gmail.com>
2023-10-15 16:26:46 -0700

    unsigned

--- printf_unsigned.c
+++ printf_unsigned.c
@@ -1,15 +1,19 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
  * printf_unsigned - Print an unsigned integer.
+ *
  * @args: Argument to print.
+ *
  * Return: Number of characters printed.
  */
+
 int printf_unsigned(va_list args)
 {
-    unsigned int n = va_arg(args, unsigned int);
-    unsigned int digit, exp = 1;
-    int i = 1;
+	unsigned int n = va_arg(args, unsigned int);
+	unsigned int digit, exp = 1;
+	int i = 1;
 
 	while (exp <= n / 10)
 		exp *= 10;
@@ -23,5 +27,5 @@ int printf_unsigned(va_list args)
 		i++;
 	}
 
-	return i;
+	return (i);
 }
